af13d29c6b51d559202af1ee894961f1ee6212c6
Naomi Cox <naomicox@mail.weber.edu>
2018-11-27 09:50:22 -0700

    Tuesday work

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,23 +2,23 @@
     <nav class="navbar navbar-default">
         <div class="container-fluid">
             <div class="navbar-header">
-                <!--ToDo: Create a router-link that goes to '/' root, add class navbar-brand and add Stock Trader text-->
+                <router-link to="/" class="navbar-brand"> Stock Trader </router-link>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <!--ToDo: Create router-link that goes to '/portfolio'
-                        <!--ToDo: set activeClass to "active", add tag attribute set to li, and add Portfolio text-->
+                    <router-link to="/portfolio" active-class="active" tag="li" exact> Portfolio </router-link>
 
-                    <!--ToDo: Create router-link that goes to '/stocks'
-                        <!--ToDo: set activeClass to "active", add tag attribute set to li, and add Stocks text-->
+                    <router-link to="/stocks" active-class="active" tag="li"> Stocks </router-link>
+
                 </ul>
                 <strong class="navbar-text navbar-right">Funds:
+                    {{funds | currency }}
                     <!--ToDo: Call funds computed function and pipe the currency filter that is created in main.js-->
                 </strong>
                 <ul class="nav navbar-nav navbar-right">
                     <li>
                         <!--ToDo: Add click event to <a> that calls endDay method-->
-                        <a href="#" >End Day</a>
+                        <a href="#"  @click="endDay">End Day</a>
                     </li>
 
                     <!--ToDo: Inside <li> Bind to class using :class that passes an object {} called open and set it to isDropdownOpen-->
@@ -44,10 +44,14 @@
     export default {
         data() {
           return {
+            isDropdownOpen = false;
               //ToDo: Create data object called isDropdownOpen and set it to false
           }
         },
         computed: {
+          funds(){
+                return this.$store.getters.funds;
+           }
         //ToDo: Create a computed function called funds
             //ToDo: Have funds() return this.$store.getters.funds
         },
@@ -55,7 +59,9 @@
             //ToDo: Create ...mapActions method
                 //ToDo: Call randomizeStocks: 'randomizeStocks'
                 //ToDo: Call fetchData: 'loadData'
-
+            endDay(){
+              randomizeStocks()
+            }
             //ToDo: Create endDay method
                 //ToDo: Call randomizeStocks()
 
