aa14263c9d5f7e3feed59d828db020191b95012e
Naomi Cox <naomicox@mail.weber.edu>
2018-12-06 07:37:28 -0700

    finishedddddd

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <app-stock v-for="stock in stocks" :stock="stock" :key="stock"></app-stock>
+        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
         <!--ToDo: Load app-stock component-->
             <!--ToDo: Loop through stock in stocks using v-for-->
             <!--ToDo: Bind to stock using : and pass stock from the v-for-->
